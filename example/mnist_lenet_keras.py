99814199ad59316e82e616fc884d6da330f15f2a
Pratyush <aspratyush@gmail.com>
2018-06-15 17:00:03 +0530

    Add train using tensorflow, update the example to use tf.model.train

--- example/mnist_lenet_keras.py
+++ example/mnist_lenet_keras.py
@@ -5,7 +5,6 @@ from __future__ import print_function
 # Imports
 import numpy as np
 import tensorflow as tf
-from keras.utils import np_utils
 from dl_utils.data import mnist
 
 # lenet_keras for keras API
@@ -13,9 +12,9 @@ from dl_utils.keras.models import lenet_keras
 # lenet_tf_keras for tf.keras API
 from dl_utils.keras.models import lenet_tf_keras
 
-from dl_utils.keras.models import train
+#from dl_utils.keras.models import train
 from dl_utils.tf.estimators import keras_estimator
-
+from dl_utils.tf.models import train
 # Log level
 tf.logging.set_verbosity(tf.logging.DEBUG)
 
@@ -23,7 +22,12 @@ tf.logging.set_verbosity(tf.logging.DEBUG)
 def main():
 
     # Constant values
+    NB_EPOCHS = 10
+    NB_BATCHES = 128
     NB_CLASSES = 10
+    NB_ROWS = 28
+    NB_COLS = 28
+    NB_CHANNELS = 1
 
     # 1. Loading the data
     print('Loading data...')
@@ -36,14 +40,18 @@ def main():
     #3. get one-hot encoded vectors for the labels
     Y_oh = {}
     for key in Y.keys():
-        Y_oh[key] = np_utils.to_categorical(Y[key], NB_CLASSES)
+        Y_oh[key] = tf.keras.utils.to_categorical(Y[key], NB_CLASSES)
     
     # 4. train
     ## a. keras
     #train.run(model, X, Y_oh, nb_epochs=5)
 
     ## b. estimator
-    keras_estimator.run(model, X, Y_oh, nb_epochs=30, nb_batches=128)
+    #keras_estimator.run(model, X, Y_oh, nb_epochs=30, nb_batches=128)
+
+    ## c. tf
+    train.run(model, X, Y_oh, model_path='./saved_model/lenet',nb_epochs=NB_EPOCHS, nb_batches=NB_BATCHES, nb_rows=NB_ROWS, 
+            nb_cols=NB_COLS, nb_channels=NB_CHANNELS, nb_classes=NB_CLASSES)
 
 if __name__ == "__main__":
   #tf.app.run()
---
+++ tf/models/train.py
@@ -0,0 +1,80 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+# Imports
+import tensorflow as tf
+import math
+from sklearn.utils import shuffle
+
+def run(model, X, Y, model_path = '/tmp', optimizer=None, 
+        nb_epochs=30, nb_batches=128, nb_rows=28, nb_cols=28, nb_channels=1, nb_classes=10):
+
+    # define the placeholders                             
+    x = tf.placeholder(tf.float32, shape=(None, nb_rows, nb_cols, nb_channels))
+    y = tf.placeholder(tf.float32, shape=(None, nb_classes))
+
+    # get the prediction tensor
+    y_pred = model(x)
+
+    # 1. loss function
+    with tf.name_scope('loss'):
+        cross_entropy = tf.losses.softmax_cross_entropy(y, y_pred)
+        cross_entropy = tf.reduce_mean(cross_entropy)
+    
+    # 2. optimizer
+    with tf.name_scope('model_optimizer'):
+        if (optimizer is None):
+            train_step = tf.train.AdamOptimizer(1e-3).minimize(cross_entropy)
+        else:
+            # TODO check!!!
+            train_step = optimizer.minimize(cross_entropy)
+    
+    # 3. accuracy
+    with tf.name_scope('accuracy'):
+        correct_prediction = tf.equal(tf.argmax(y_pred, 1), tf.argmax(y,1))
+        correct_prediction = tf.cast(correct_prediction, tf.float32)
+        accuracy = tf.reduce_mean(correct_prediction)
+    
+    # 4. run the training
+    saver = tf.train.Saver()
+
+    with tf.Session() as sess:
+        # intialize variables
+        sess.run(tf.global_variables_initializer())
+        
+        # steps
+        nb_steps = math.floor(X['train'].shape[0]/nb_batches)
+        
+        X_train = X['train']
+        Y_train = Y['train']
+        if (Y_train.shape[1] != nb_classes):
+            Y_train = tf.keras.utils.to_categorical(Y_train, nb_classes)
+
+        # iterate over the epochs
+        print('Training...')
+        for epoch in range(nb_epochs):
+
+            # shuffle
+            X_train, Y_train = shuffle(X_train, Y_train)
+        
+            # Take the steps
+            for i in range(nb_steps):
+            
+                # Extract the batch
+                x_batch = X_train[i*nb_batches:(i+1)*nb_batches]
+                y_batch = Y_train[i*nb_batches:(i+1)*nb_batches]
+
+                # run the optimizer
+                sess.run(train_step, feed_dict={x:x_batch, y:y_batch})
+            
+            # 5. Validation
+            # print accuracy every 10 epochs 
+            if (epoch % 2 == 0):
+                val_accuracy = sess.run(accuracy, feed_dict={x: X['val'], y: Y['val']})
+                val_loss = sess.run(cross_entropy, feed_dict={x: X['val'], y: Y['val']})
+                print('Epoch: ', epoch, ', Val accuracy: ', val_accuracy, ', Val loss : ', val_loss)
+
+        # Save the model
+        print('Saving the model...')
+        saver.save(sess, model_path)
